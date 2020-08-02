52a9a3b61f0fcb27e9d44b79497d6f7af39dd32b
Septs <github@septs.pw>
2020-08-02 22:29:15 +0800

    simplify code

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
+# docker-gun
+
+## docker-compose.yaml (sample)
+
+```yaml
 version: "3"
 
 services:
   gundb:
-    build: .
+    image: dimensiondev/gun:latest
     restart: always
     ports:
       - "127.0.0.1:8765:8765"
     volumes:
       - ./data:/app/data
+```
--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-const cluster = require("cluster");
-if (cluster.isMaster) {
-  cluster.fork();
-  cluster.on("exit", () => {
-    cluster.fork();
-    require("gun/lib/crashed");
-  });
-} else {
-  const PORT = 8765;
+const fs = require("fs");
+const path = require("path");
+const Gun = require("gun");
 
-  const fs = require("fs");
-  const path = require("path");
-  const Gun = require("gun");
-  const files = path.join(__dirname, "files");
-  const resources = fs.existsSync(files)
-    ? files
-    : path.dirname(require.resolve("gun/examples/http"));
-  Gun({
-    web: require("http").createServer(Gun.serve(resources)).listen(PORT),
-    multicast: false,
-  });
-  console.log(`Relay peer started on port ${PORT} with /gun`);
-}
+const PORT = 8765;
+
+const files = path.join(__dirname, "files");
+const resources = fs.existsSync(files)
+  ? files
+  : path.dirname(require.resolve("gun/examples/http"));
+
+Gun({
+  web: require("http").createServer(Gun.serve(resources)).listen(PORT),
+  multicast: false,
+});
+
+console.log(`Relay peer started on port ${PORT} with /gun`);
