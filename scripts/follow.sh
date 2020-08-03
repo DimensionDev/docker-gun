e835c70e7a31cec235fa4b4a4847b46a0284e338
Septs <github@septs.pw>
2020-08-03 14:08:11 +0800

    update scripts

--- scripts/follow.sh
+++ scripts/follow.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
 set -x
 npm install gun@latest
 if [[ -z $(git status -s) ]]; then
 	echo "no new version found."
 	exit 0
+else
+	VERSION=$(npm info gun@latest version)
+	git add .
+	git commit -m "new version: $VERSION"
+	git tag -a "$VERSION" -m "new version: $VERSION"
+	git push --tags
 fi
-VERSION=$(npm info gun version)
-git add .
-git commit -m "new version: $VERSION"
-git tag -a "$VERSION" -m "new version: $VERSION"
-git push --tags
