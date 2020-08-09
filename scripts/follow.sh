6e6cc677358dde27809498e3d9124d1fc7074dda
Septs <github@septs.pw>
2020-08-09 09:18:27 +0800

    update scripts

--- scripts/follow.sh
+++ scripts/follow.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
 set -x
 npm install gun@latest
-if [[ -z $(git status -s) ]]; then
+
+git diff-index --quiet HEAD
+if [[ "$?" == "0" ]]; then
 	echo "no new version found."
-else
-	VERSION=$(npm info gun@latest version)
-	git config user.name "Dimension Bot"
-	git config user.email "bot@dimension.im"
-	git fetch --tags
-	git add .
-	git commit --message "new version: $VERSION"
-	git tag --force --annotate "$VERSION" --message "new version: $VERSION"
-	git push --force --tags
-	git push
+	exit 1
 fi
+
+set -o pipefail
+VERSION=$(npm info gun@latest version)
+git config user.name "Dimension Bot"
+git config user.email "bot@dimension.im"
+git fetch --tags
+git add .
+git commit --message "new version: $VERSION"
+git tag --force --annotate "$VERSION" --message "new version: $VERSION"
+git push
+git push --follow-tags
