7d32e80936b0b19a44a160d2d0de4442d0ee0f9f
Septs <github@septs.pw>
2021-02-05 07:29:57 +0800

    update follow script

--- scripts/follow.sh
+++ scripts/follow.sh
@@ -10,8 +10,8 @@ fi
 
 set -o pipefail
 VERSION=$(npm info gun@latest version)
-git config user.name "github-actions"
-git config user.email "github-actions@github.com"
+git config user.name "github-actions[bot]"
+git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git fetch --tags
 git add .
 git commit --message "new version: $VERSION"
